404e2f02f87fb0fb869525dcc27ddbf57527788f
pbarbarone <peter.barbarone@gmail.com>
2018-02-22 10:19:20 -0800

    bug occuring when trying to pull a bird without img fixed. commenting code and styling

--- controllers/testCode.js
+++ controllers/testCode.js
@@ -1,97 +1,302 @@
-// T.post('statuses/update', { status: 'tweet tweet yall' }, function(err, data, response) {
-//   console.log(data)
+// // T.post('statuses/update', { status: 'tweet tweet yall' }, function(err, data, response) {
+// //   console.log(data)
+// // })
+
+// // function getBase64Image(img) {
+// //   var canvas = document.createElement("canvas");
+// //   canvas.width = img.width;
+// //   canvas.height = img.height;
+// //   var ctx = canvas.getContext("2d");
+// //   ctx.drawImage(img, 0, 0);
+// //   var dataURL = canvas.toDataURL("image/png");
+// //   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+// // }
+// // var base64 = getBase64Image('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg/220px-Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg');
+
+
+// // console.log(base53);
+// // module.exports = router;
+
+// // var imgPath = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg/220px-Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg'
+
+// // request.get('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg/220px-Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg', function (error, response, body) {
+// //     if (!error && response.statusCode == 200) {
+// //         // console.log('daataaaa' + body.data)
+// //         let dataTwo =  "";
+// //         var reader = new FileReader();
+// //         reader.readAsDataURL(imgPath);
+// //         reader.onload = function (e){
+// //         dataTwo = new Buffer(e.target.result, "base64");
+// //         console.log(dataTwo);
+
+// //         }
+// //         T.post('media/upload',{ 
+// //           media_data: dataTwo
+// //         }, function (err, data, response) {
+// //           // now we can assign alt text to the media, for use by screen readers and 
+// //           // other text-based presentations and interpreters 
+// //           var mediaIdStr = data.media_id_string;
+// //           console.log('response ' + response.statusCode)
+// //           console.log('media ' + mediaIdStr);
+// //           var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+// //           var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+// //           console.log("initial post " + mediaIdStr);
+// //           if (err){
+// //             console.log('error, yo ' + err)
+// //           }
+          
+// //           T.post('media/metadata/create', meta_params, function (err, data, response) {
+// //             if (!err) {
+// //               // now we can reference the media and post a tweet (media will attach to the tweet) 
+// //               var params = { status: 'birds, man', media_ids: [mediaIdStr] }
+         
+// //              T.post('statuses/update', params, function (err, data, response) {
+// //                 console.log(data)
+// //               }) 
+// //             }
+// //        })
+// //     })
+// //   }
+// // });
+
+// // function getBase64Image(img) {
+// //   var canvas = document.createElement("canvas");
+// //   canvas.width = img.width;
+// //   canvas.height = img.height;
+// //   var ctx = canvas.getContext("2d");
+// //   ctx.drawImage(img, 0, 0);
+// //   var dataURL = canvas.toDataURL("image/png");
+// //   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+// //  }
+ 
+// //  var base64 = getBase64Image(imgPath);
+// //  console.log(base64);
+
+
+// var b64content = fs.readFileSync('../bird.jpg', { encoding: 'base64' })
+// console.log(b64content);
+ 
+// // first we must post the media to Twitter 
+// T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+//   // now we can assign alt text to the media, for use by screen readers and 
+//   // other text-based presentations and interpreters 
+//   var mediaIdStr = data.media_id_string
+//   var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+//   var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+ 
+//   T.post('media/metadata/create', meta_params, function (err, data, response) {
+//     if (!err) {
+//       // now we can reference the media and post a tweet (media will attach to the tweet) 
+//       var params = { status: 'col hates birds', media_ids: [mediaIdStr] }
+ 
+//       T.post('statuses/update', params, function (err, data, response) {
+//         console.log(data)
+//       })
+//     }
+//   })
 // })
 
-// function getBase64Image(img) {
-//   var canvas = document.createElement("canvas");
-//   canvas.width = img.width;
-//   canvas.height = img.height;
-//   var ctx = canvas.getContext("2d");
-//   ctx.drawImage(img, 0, 0);
-//   var dataURL = canvas.toDataURL("image/png");
-//   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-// }
-// var base64 = getBase64Image('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg/220px-Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg');
 
+[
+  243,
+  238,
+  242,
+  245,
+  239,
+  241,
+  246,
+  261,
+  275,
+  251,
+  256,
+  266,
+  280,
+  313,
+  292,
+  286,
+  306,
+  322,
+  326,
+  332,
+  337,
+  342,
+  352,
+  369,
+  374,
+  393,
+  409,
+  404,
+  414,
+  418,
+  424,
+  429,
+  258,
+  264,
+  268,
+  248,
+  252,
+  272,
+  277,
+  282,
+  290,
+  285,
+  296,
+  304,
+  309,
+  300,
+  323,
+  328,
+  333,
+  339,
+  344,
+  355,
+  351,
+  348,
+  370,
+  360,
+  375,
+  387,
+  398,
+  413,
+  408,
+  419,
+  430,
+  247,
+  263,
+  259,
+  253,
+  273,
+  270,
+  312,
+  288,
+  293,
+  302,
+  307,
+  321,
+  331,
+  336,
+  359,
+  368,
+  349,
+  379,
+  391,
+  389,
+  402,
+  407,
+  417,
+  422,
+  427,
+  254,
+  276,
+  267,
+  271,
+  260,
+  249,
+  262,
+  281,
+  287,
+  308,
+  298,
+  291,
+  295,
+  303,
+  316,
+  320,
+  329,
+  325,
+  335,
+  340,
+  345,
+  361,
+  366,
+  385,
+  395,
+  416,
+  433,
+  423,
+  274,
+  269,
+  255,
+  257,
+  265,
+  279,
+  314,
+  311,
+  294,
+  299,
+  305,
+  284,
+  318,
+  289,
+  324,
+  334,
+  330,
+  338,
+  343,
+  347,
+  353,
+  362,
+  357,
+  392,
+  401,
+  406,
+  415,
+  397,
+  426,
+  421,
+  434,
+  436,
+  440,
+  445,
+  442,
+  444,
+  446,
+  453,
+  449,
+  447,
+  456,
+  451,
+  458,
+  452,
+  457,
+  455,
+  460,
+  462,
+  459,
+  465,
+  470,
+  466,
+  469,
+  467,
+  468,
+  473,
+  475,
+  476,
+  478,
+  479,
+  480,
+  244,
+  240
+  ]
+var db = require('../models');
 
-// console.log(base53);
-// module.exports = router;
+var goodBirds = [];
+function getGoodBirds (){
+    db.bird.findAll({
+    }).then(function(bird){
+      console.log('bird length' + bird.length)
+      for(var i = 0; i<=bird.length -1; i++){
+        if(!bird[i].imgUrl){
+          console.log(bird[i] + ' has no img');
+        } else if(bird[i].imgUrl){
+          goodBirds.push(bird[i].id);
+        } else {
+          console.log('something wrong');
+        }
+      }
+      console.log(goodBirds);
+    });
+    return goodBirds;
+}
 
-// var imgPath = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg/220px-Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg'
-
-// request.get('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg/220px-Yellow-billed_Loon_Chipp_South_8-12-13_Ryan_Askren.jpg', function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//         // console.log('daataaaa' + body.data)
-//         let dataTwo =  "";
-//         var reader = new FileReader();
-//         reader.readAsDataURL(imgPath);
-//         reader.onload = function (e){
-//         dataTwo = new Buffer(e.target.result, "base64");
-//         console.log(dataTwo);
-
-//         }
-//         T.post('media/upload',{ 
-//           media_data: dataTwo
-//         }, function (err, data, response) {
-//           // now we can assign alt text to the media, for use by screen readers and 
-//           // other text-based presentations and interpreters 
-//           var mediaIdStr = data.media_id_string;
-//           console.log('response ' + response.statusCode)
-//           console.log('media ' + mediaIdStr);
-//           var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-//           var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
-//           console.log("initial post " + mediaIdStr);
-//           if (err){
-//             console.log('error, yo ' + err)
-//           }
-          
-//           T.post('media/metadata/create', meta_params, function (err, data, response) {
-//             if (!err) {
-//               // now we can reference the media and post a tweet (media will attach to the tweet) 
-//               var params = { status: 'birds, man', media_ids: [mediaIdStr] }
-         
-//              T.post('statuses/update', params, function (err, data, response) {
-//                 console.log(data)
-//               }) 
-//             }
-//        })
-//     })
-//   }
-// });
-
-// function getBase64Image(img) {
-//   var canvas = document.createElement("canvas");
-//   canvas.width = img.width;
-//   canvas.height = img.height;
-//   var ctx = canvas.getContext("2d");
-//   ctx.drawImage(img, 0, 0);
-//   var dataURL = canvas.toDataURL("image/png");
-//   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-//  }
- 
-//  var base64 = getBase64Image(imgPath);
-//  console.log(base64);
-
-
-var b64content = fs.readFileSync('../bird.jpg', { encoding: 'base64' })
-console.log(b64content);
- 
-// first we must post the media to Twitter 
-T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-  // now we can assign alt text to the media, for use by screen readers and 
-  // other text-based presentations and interpreters 
-  var mediaIdStr = data.media_id_string
-  var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-  var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
- 
-  T.post('media/metadata/create', meta_params, function (err, data, response) {
-    if (!err) {
-      // now we can reference the media and post a tweet (media will attach to the tweet) 
-      var params = { status: 'col hates birds', media_ids: [mediaIdStr] }
- 
-      T.post('statuses/update', params, function (err, data, response) {
-        console.log(data)
-      })
-    }
-  })
-})
+getGoodBirds();
--- controllers/twit.js
+++ controllers/twit.js
@@ -9,54 +9,26 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var download = require('image-downloader')
+var goodBirds = require('../public/goodBirds.js');
 
-
-
-
-// console.log(config)
-
+// config file contains my twitter verification items
 var T = new Twit(config);
 
-// var b64content = fs.readFileSync('../bird.jpg', { encoding: 'base64' })
-// console.log(b64content);
- 
-// // first we must post the media to Twitter 
-// T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-//   // now we can assign alt text to the media, for use by screen readers and 
-//   // other text-based presentations and interpreters 
-//   var mediaIdStr = data.media_id_string
-//   var altText = "Small flowers in a planter on a sunny balcony, blossoming."
-//   var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
- 
-//   T.post('media/metadata/create', meta_params, function (err, data, response) {
-//     if (!err) {
-//       // now we can reference the media and post a tweet (media will attach to the tweet) 
-//       var params = { status: 'col hates birds', media_ids: [mediaIdStr] }
- 
-//       T.post('statuses/update', params, function (err, data, response) {
-//         console.log(data)
-//       })
-//     }
-//   })
-// })
+router.get('/', function(req, res){
+    //importing array from goodBirds.js that contains only clean db data ie birds with imgs  
+    // array contains their id, grabs one at random index of said array  
+    var randomId =  goodBirds[Math.floor(goodBirds.length * Math.random())];
 
-router.get('/', function(req, res){    
-    var randomId = Math.floor(238 + Math.random() * 243);
+    //query DB via random good bird id
     db.bird.findOne({
       where: {id: randomId},
     }).then(function(bird){
-      if(!bird.imgUrl){
-        console.log(bird.imgUrl);
-        console.log(bird.comName, bird.sciName, randomId);
-        findBird();
-      } else {
-        console.log('success');
-        console.log("What is my name?");
-        console.log(bird.id)
+        //img file name corresponds with bird's id, this is converted to base64 below, which twitter requires
         var b64content = fs.readFileSync('./birdImgs/'+ bird.id +'.jpg', { encoding: 'base64' })
-        console.log(b64content);
-        // res.render('home', {bird: bird});
+
+        //this is is all the info my front end needs to render how i would like, but more is needed to functionally tweet
         res.render('home',{bird: bird});
+        //initial submit of photo file to twitter to get "approved"
         T.post('media/upload', { media_data: b64content }, function (err, data, response) {
           // now we can assign alt text to the media, for use by screen readers and 
           // other text-based presentations and interpreters 
@@ -67,16 +39,18 @@ router.get('/', function(req, res){
           T.post('media/metadata/create', meta_params, function (err, data, response) {
             if (!err) {
               // now we can reference the media and post a tweet (media will attach to the tweet) 
+              // final step of tweeting, where the post finally happens. 
               var params = { status: 'Common Name: '+ bird.comName + ' Scientific Name: ' + bird.sciName, media_ids: [mediaIdStr] }
-         
               T.post('statuses/update', params, function(err, data, response) {
               });
             }
           });
         });
-      };
     });
 });
+
+//imgGet is the "seeding" function that combs my DB for birds with actual imgUrls
+// using the module image-downloader, it then downloads succesful url gets and saves them locally with the file name of the bird id 
 
 // function imgGet (){
 //   for(let i = 238; i <= 481; i++){
@@ -103,9 +77,6 @@ router.get('/', function(req, res){
 // imgGet()
 
 
-
-console.log('test');
-// ../birdimgs/bird.jpg
 
 module.exports = router;
 
